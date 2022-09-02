9a642725287e0eb194bcd060a988553c11e10d2e
desarrolloWorkFlowTeams <112507097+desarrolloWorkFlowTeams@users.noreply.github.com>
2022-09-02 13:13:15 -0500

    Delete src/environments directory

--- src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: true,
-  logoUriVolquetas: 'https://www.urivolquetas.com/wp-content/uploads/2021/11/logo-urivolquetas-153x72.png',
-  bitrixUriVolquetas: 'https://urivolquetas.bitrix24.co/rest/21/feoi7exndf9d56a6/',
-  appURL:'localhost:4200',
-  app_ID: '',
-  authURL: 'https://urivolquetas.bitrix24.com/oauth/authorize',
-  crm: 'https://urivolquetas.bitrix24.co/rest/21/feoi7exndf9d56a6'
-};
