92430bbc1e092bd478e058b52aba96c96903857e
Luillin Escobar <cataplum.17@hotmail.com>
2022-09-12 20:00:04 -0500

    se agrega metodo de subir imagen

--- src/app/modules/programming/components/search/search.component.ts
+++ src/app/modules/programming/components/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {CrmService} from "../../../core/services/crm.service";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {Router} from "@angular/router";
@@ -10,16 +10,17 @@ import {Router} from "@angular/router";
 })
 export class SearchComponent implements OnInit {
   searchForm!: FormGroup;
-  id:string= "265";
-  placas: any[] =[];
+  id: string = "265";
+  placas: any[] = [];
   deals: any [] = [];
   state = 'C7:PREPARATION';
   campos: any = {};
+
   constructor(
     private readonly crm: CrmService,
     private readonly formBuilder: FormBuilder,
     private readonly router: Router
-    ) {
+  ) {
   }
 
   ngOnInit(): void {
@@ -28,19 +29,6 @@ export class SearchComponent implements OnInit {
     }
     this.searchForm = this.formBuilder.group(this.campos)
     this.traerPlacas();
-    // this.crm.getDealForId('crm.deal.get', '1483').subscribe({
-    //   'next': (deal: any) => {
-    //     console.log(('_').repeat(50))
-    //     console.log(deal.result)
-    //     console.log(('_').repeat(50))
-    //   }
-    // })
-    let start = 0;
-    let next = true
-    // while(next){
-    //
-    //   start = start + 50;
-    // }
     let options = {
       filter: {'STAGE_ID': `${this.state}`, 'UF_CRM_1659706567283': "SNX609"},
       select: ['UF_CRM_1659706567283', 'STAGE_ID']
@@ -48,45 +36,27 @@ export class SearchComponent implements OnInit {
     this.crm.getDealList(0, options).subscribe({
       'next': (deal: any) => {
         console.log(deal)
-        // deal.result.forEach((item: any)=> {
-        //   //if(item.hasOwnProperty('UF_CRM_1659706567283'))
-        //   this.deals.push(item)
-        // })
       },
       'error': error => console.log(error),
     })
-    console.log({deals: this.deals});
   }
 
-
-
-  // getDataFromForm(form: any){
-  //   const { placa } = form.value
-  //   let options = {
-  //     filter: {'STAGE_ID': `${this.state}`, 'UF_CRM_1659706567283': `${placa}`},
-  //     select: ['UF_CRM_1659706567283', 'STAGE_ID']
-  //   };
-  // }
-
   traerPlacas(event?: any) {
-    console.log(event)
     let options = {
-          filter: { 'UF_CRM_1659061343591': `${this.id}`},
-        };
+      filter: {'UF_CRM_1659061343591': `${this.id}`},
+    };
 
     this.crm.getCompanyList(`${this.id}`, options).subscribe({
-      'next': (companies: any) =>{
-        console.log('_____companies______',companies)
-
+      'next': (companies: any) => {
         this.placas = companies.result;
       }
     });
   }
 
   searchRequest() {
-    console.log(this.searchForm.value);
-    this.router.navigate(['/programming/result-search'], {queryParams: { placa: this.searchForm.value.placa }}).then()
+    if (this.searchForm.valid) {
+      this.router.navigate(['/programming/result-search'], {queryParams: {placa: this.searchForm.value.placa}}).then()
+    }
   }
 }
-// UF_CRM_1659706567283
-// STAGE_ID: C7:PREPARATION
+
