819471a0a663e91c9d99dc530e4326743df17556
Andrey <andreyduque1987@gmail.com>
2022-09-19 13:25:32 -0500

    placas conductor y maquina ok

--- src/app/modules/programming/components/search/search.component.ts
+++ src/app/modules/programming/components/search/search.component.ts
@@ -1,7 +1,7 @@
-import {Component, OnInit} from '@angular/core';
-import {CrmService} from "../../../core/services/crm.service";
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {Router} from "@angular/router";
+import { Component, OnInit } from '@angular/core';
+import { CrmService } from "../../../core/services/crm.service";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
 import Swal from 'sweetalert2';
 
 @Component({
@@ -11,9 +11,12 @@ import Swal from 'sweetalert2';
 })
 export class SearchComponent implements OnInit {
   searchForm!: FormGroup;
-  id: string = "265";
+  idConductor: string = "265";
+  idMaquina: string = "289";
+  optionsSelect: any;
+  resulCompanies: any;
   placas: any[] = [];
-  deals: any [] = [];
+  deals: any[] = [];
   state = 'C7:PREPARATION';
   campos: any = {};
 
@@ -31,7 +34,7 @@ export class SearchComponent implements OnInit {
     this.searchForm = this.formBuilder.group(this.campos)
     this.traerPlacas();
     let options = {
-      filter: {'STAGE_ID': `${this.state}`, 'UF_CRM_1659706567283': "SNX609"},
+      filter: { 'STAGE_ID': `${this.state}`, 'UF_CRM_1659706567283': "SNX609" },
       select: ['UF_CRM_1659706567283', 'STAGE_ID']
     };
     this.crm.getDealList(0, options).subscribe({
@@ -43,20 +46,30 @@ export class SearchComponent implements OnInit {
   }
 
   traerPlacas(event?: any) {
-    let options = {
-      filter: {'UF_CRM_1659061343591': `${this.id}`},
-    };
-
-    this.crm.getCompanyList(`${this.id}`, options).subscribe({
-      'next': (companies: any) => {
-        this.placas = companies.result;
+    this.optionsSelect = [
+      {
+        filter: { 'UF_CRM_1659061343591': `${this.idConductor}` }
+      },
+      {
+        filter: { 'UF_CRM_1659061343591': `${this.idMaquina}` }
       }
-    });
+    ];
+
+    for (let index = 0; index < this.optionsSelect.length; index++) {
+      this.crm.getCompanyList(`${this.optionsSelect[index].filter.UF_CRM_1659061343591}`, this.optionsSelect[index]).subscribe({
+        'next': (companies: any) => {
+          this.resulCompanies = companies.result;
+          for (let i = 0; i < this.resulCompanies.length; i++) {
+            this.placas.push(this.resulCompanies[i].TITLE);
+          }
+        }
+      });
+    }
   }
 
   searchRequest() {
     if (this.searchForm.valid) {
-      this.router.navigate(['/programming/result-search'], {queryParams: {placa: this.searchForm.value.placa}}).then()
+      this.router.navigate(['/programming/result-search'], { queryParams: { placa: this.searchForm.value.placa } }).then()
     } else {
       Swal.fire({
         icon: 'error',
